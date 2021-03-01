f88c143ac4cb233303cd63e27af1139d9b48b067
povle <pasha@blinov.co>
2021-03-01 14:51:48 +0300

    create tables if they do not exist

--- whiteboard/__init__.py
+++ whiteboard/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_socketio import SocketIO
-from .database import db_wrapper
+from .database import db_wrapper, Board, Stroke
 import logging.config
 logging.config.fileConfig('logger.conf')
 socketio = SocketIO(engineio_logger=True, cors_allowed_origins="*")
@@ -11,6 +11,7 @@ def create_app():
     app.config.from_pyfile('../config.py', silent=True)
 
     db_wrapper.init_app(app)
-    socketio.init_app(app)
+    db_wrapper.database.create_tables([Board, Stroke])
 
+    socketio.init_app(app)
     return app
