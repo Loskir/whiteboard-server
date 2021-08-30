c1018dfd78276c3fb397be6c7fb305598552b0b3
Loskir <kl@sinker.ru>
2021-08-30 16:03:20 +0300

    Add docker

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/bin/env python
+from dotenv import load_dotenv
+import os
+
 from whiteboard import create_app, socketio
+
+load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=False, port=10000, host='0.0.0.0')
+    socketio.run(app, debug=False, port=int(os.getenv('PORT', '10000')), host=os.getenv('HOST', '0.0.0.0'))
